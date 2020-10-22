385de75664bb47eba107fb6bfec16ee903ffb9f9
Andrew Chaiko <andrewc@playtika.com>
2020-10-22 12:23:53 +0300

    Code to execute via animation event

---
+++ Assets/GameCode/JumpingCubeView.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Academy.Animations
+{
+    [RequireComponent(typeof(Animation))]
+    public class JumpingCubeView : MonoBehaviour
+    {
+        [SerializeField]
+        private ParticleSystem _particleSystem;
+        
+        private Animation _animation;
+
+        private void Awake()
+        {
+            _animation = GetComponent<Animation>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                _animation[_animation.clip.name].speed *= -1;
+            }
+        }
+
+        private void PlayPuff()
+        {
+            _particleSystem.Play();
+        }
+    }
+}
