b09020841daefd4191a7e872fa3fdae9c06b2b1c
VeremyevVadim <qwertyvadimv@mail.ru>
2020-10-23 11:18:10 +0300

    Revert "Revert "My super jump animation""
    This reverts commit fcb95015cd7a886a305e4889ca0ee4933e9881f9.

---
+++ Assets/Art/JumpBoyEventController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Animation))]
+public class JumpBoyEventController : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _particleSystemStartJump;
+    [SerializeField] private ParticleSystem _particleSystemOnFly;
+
+    private void OnJump()
+    {
+        _particleSystemStartJump.Play();
+        _particleSystemOnFly.Play();
+    }
+    
+}
